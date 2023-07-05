2c4ed5bfc9b063a07be73aab0f74b3be2675ad43
belovetech <belovetech@gmail.com>
2023-07-05 10:54:07 +0100

    [Update]: healthcheck

--- auth_service/src/controllers/healthcheck.controller.ts
+++ auth_service/src/controllers/healthcheck.controller.ts
@@ -8,7 +8,8 @@ export default class HealthCheck {
       if (mongoClient.isAlive() && redisClient.isAlive()) {
         return res.status(200).json({ message: 'Pong!' });
       }
-      return res.status(200).json({
+      return res.status(500).json({
+        message: 'Connection is down!',
         mongoClient: mongoClient.isAlive(),
         redisClient: redisClient.isAlive(),
       });
