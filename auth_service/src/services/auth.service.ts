0180690c7c91d351747cd3bf2808fdd4980afdfd
belovetech <belovetech@gmail.com>
2023-07-14 19:53:08 +0100

    [Update]: create generate and verify OTP

--- auth_service/src/services/auth.service.ts
+++ auth_service/src/services/auth.service.ts
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt';
 import Customer from '@models/customers.model';
-import { AuthenticationException, ConflictException, NotFoundException } from '@exceptions';
+import { AuthenticationException, BadRequestException, ConflictException, NotFoundException } from '@exceptions';
 import { CustomerDataValidator } from '@validators/customerPayloadValidator';
 import { ICustomer, IPayload } from '@interfaces/customers.interface';
 import redisClient from '@datasource/redis';
+import VerifyOtpDto from '@dtos/verifyOtp.dto';
 
 export default class AuthService {
   public static async signup(payload: IPayload): Promise<ICustomer> {
@@ -13,8 +14,8 @@ export default class AuthService {
     if (customerExist) throw new ConflictException();
     const customer = await Customer.create({ ...payload });
 
-    const otp = this.generateOTP();
-    const key = `otp_${customer.customerId}`;
+    const otp = await bcrypt.hash(JSON.stringify(this.generateOTP()), 2);
+    const key = `otp_${customer.email}`;
     await redisClient.del(key);
     redisClient.set(key, otp, 60 * 10); // 10mins
     // send otp to the customer's email
@@ -29,6 +30,19 @@ export default class AuthService {
     if (!isCorrectPassword) throw new AuthenticationException();
     // generate auth token
     // store token in the redis server
+  }
+
+  public static async verifyOtp(data: VerifyOtpDto): Promise<ICustomer> {
+    if (!data) throw new BadRequestException('Invalid OTP credentials');
+    const storedOtp = await redisClient.get(`otp_${data.email}`);
+    if (!storedOtp) throw new AuthenticationException('OTP token has expired');
+    const validOtp = await bcrypt.compare(data.otp, storedOtp);
+    if (!validOtp) throw new AuthenticationException('Invalid OTP token');
+    const customer = await Customer.findOne({ email: data.email }).exec();
+    await redisClient.del(`otp_${data.email}`);
+    customer.isVerified = true;
+    customer.save();
+    return customer;
   }
 
   private static generateOTP(): number {
