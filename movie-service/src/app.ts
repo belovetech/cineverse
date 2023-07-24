2127d6948bb90dd98222b5229943d8ddeecdca1f
belovetech <belovetech@gmail.com>
2023-07-24 12:57:02 +0100

    [Update]: clean tsconfig

--- movie-service/src/app.ts
+++ movie-service/src/app.ts
@@ -1,26 +1,23 @@
 import express, { Application } from 'express';
 import config from '@config';
 import IRoute from '@interfaces/route.interface';
-import { logger } from '@cineverse/logger';
 
 export default class App {
   private app: Application;
   private port: string | number;
-  private env: string;
 
   constructor(routes: IRoute[]) {
     this.app = express();
     this.port = config.development.port || 5000;
-    this.env = config.node_env || 'development';
 
     this.initializeRoutes(routes);
   }
 
   public listen(): void {
     this.app.listen(this.port, () => {
-      logger.info('==================================');
-      logger.info(`App listening on localhost:${this.port} 🚀`);
-      logger.info('==================================');
+      console.info('==================================');
+      console.info(`App listening on localhost:${this.port} 🚀`);
+      console.info('==================================');
     });
   }
 
