0d350fb5c10b5f2885ce43c46648dfbaae3babc0
belovetech <belovetech@gmail.com>
2023-07-24 13:05:05 +0100

    [Update]: clean up logger

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,51 +1,57 @@
-import { addColors, createLogger, transports, format } from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
+import { addColors, createLogger, transports, format } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 const { combine, timestamp, colorize, printf } = format;
 
 class Logger {
   private levels = { error: 0, warn: 1, info: 2, http: 3, debug: 4 };
-  private colors = { error: 'red', warn: 'yellow', info: 'green', http: 'magenta', debug: 'white' };
-  private env = process.env.NODE_ENV || 'development';
-  private isDevelopment = this.env === 'development';
+  private colors = {
+    error: "red",
+    warn: "yellow",
+    info: "green",
+    http: "magenta",
+    debug: "white",
+  };
+  private env = process.env.NODE_ENV || "development";
+  private isDevelopment = this.env === "development";
 
   constructor() {
     addColors(this.colors);
   }
 
   private level() {
-    return this.isDevelopment ? 'debug' : 'warn';
+    return this.isDevelopment ? "debug" : "warn";
   }
 
   private logFormat() {
     return combine(
-      timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+      timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
       colorize({ all: true }),
-      printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+      printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
     );
   }
 
   private getTransports() {
     const transport1: DailyRotateFile = new DailyRotateFile({
-      filename: 'logs/app-%DATE%.log',
-      auditFile: 'logs/app.json',
-      datePattern: 'YYYY-MM-DD-HH',
+      filename: "logs/app-%DATE%.log",
+      auditFile: "logs/app.json",
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '1d',
+      maxSize: "20m",
+      maxFiles: "1d",
     });
     const transport2: DailyRotateFile = new DailyRotateFile({
-      level: 'http',
-      filename: 'logs/request-%DATE%.log',
-      auditFile: 'logs/request.json',
-      datePattern: 'YYYY-MM-DD-HH',
+      level: "http",
+      filename: "logs/request-%DATE%.log",
+      auditFile: "logs/request.json",
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '1d',
+      maxSize: "20m",
+      maxFiles: "1d",
     });
     const transport3 = new transports.File({
-      level: 'error',
-      filename: 'logs/error.log',
+      level: "error",
+      filename: "logs/error.log",
     });
     return [new transports.Console(), transport1, transport2, transport3];
   }
@@ -55,7 +61,7 @@ class Logger {
       level: this.level(),
       levels: this.levels,
       format: this.logFormat(),
-      defaultMeta: { service: 'auth-service' },
+      defaultMeta: { service: "auth-service" },
       transports: this.getTransports(),
     });
   }
