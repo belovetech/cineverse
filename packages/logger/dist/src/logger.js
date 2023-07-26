0704137be2616dfdf6284e34a3fcd6ed6b9284a4
belovetech <belovetech@gmail.com>
2023-07-26 13:50:44 +0100

    [Update]: logger transporter

--- packages/logger/dist/src/logger.js
+++ packages/logger/dist/src/logger.js
@@ -9,54 +9,36 @@ const winston_daily_rotate_file_1 = __importDefault(require("winston-daily-rotat
 const { combine, timestamp, colorize, printf } = winston_1.format;
 class Logger {
     constructor() {
-        this.levels = { error: 0, warn: 1, info: 2, http: 3, debug: 4 };
         this.colors = {
-            error: "red",
-            warn: "yellow",
-            info: "green",
-            http: "magenta",
-            debug: "white",
+            error: 'red',
+            warn: 'yellow',
+            info: 'green',
+            http: 'magenta',
+            debug: 'white',
         };
-        this.env = process.env.NODE_ENV || "development";
-        this.isDevelopment = this.env === "development";
-        (0, winston_1.addColors)(this.colors);
-    }
-    level() {
-        return this.isDevelopment ? "debug" : "warn";
+        this.env = process.env.NODE_ENV || 'development';
+        this.isDevelopment = this.env === 'development';
     }
     logFormat() {
-        return combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }), colorize({ all: true }), printf((info) => `${info.timestamp} ${info.level}: ${info.message}`));
+        return combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }), colorize({ all: true }), printf((info) => `${info.timestamp} ${info.level}: ${info.message}`));
     }
     getTransports() {
-        const transport1 = new winston_daily_rotate_file_1.default({
-            filename: "logs/app-%DATE%.log",
-            auditFile: "logs/app.json",
-            datePattern: "YYYY-MM-DD-HH",
+        const commonTransporOptions = {
+            datePattern: 'YYYY-MM-DD-HH',
             zippedArchive: true,
-            maxSize: "20m",
-            maxFiles: "1d",
-        });
-        const transport2 = new winston_daily_rotate_file_1.default({
-            level: "http",
-            filename: "logs/request-%DATE%.log",
-            auditFile: "logs/request.json",
-            datePattern: "YYYY-MM-DD-HH",
-            zippedArchive: true,
-            maxSize: "20m",
-            maxFiles: "1d",
-        });
-        const transport3 = new winston_1.transports.File({
-            level: "error",
-            filename: "logs/error.log",
-        });
+            maxSize: '50m',
+            maxFiles: '1d',
+        };
+        const transport1 = new winston_daily_rotate_file_1.default(Object.assign({ level: 'info', filename: 'logs/app-%DATE%.log', auditFile: 'logs/app.json' }, commonTransporOptions));
+        const transport2 = new winston_daily_rotate_file_1.default(Object.assign({ level: 'error', filename: 'logs/error-%DATE%.log', auditFile: 'logs/error.json' }, commonTransporOptions));
+        const transport3 = new winston_daily_rotate_file_1.default(Object.assign({ filename: 'logs/other-%DATE%.log', auditFile: 'logs/other.json' }, commonTransporOptions));
         return [new winston_1.transports.Console(), transport1, transport2, transport3];
     }
     createLogger() {
+        (0, winston_1.addColors)(this.colors);
         return (0, winston_1.createLogger)({
-            level: this.level(),
-            levels: this.levels,
+            level: this.isDevelopment ? 'debug' : 'warn',
             format: this.logFormat(),
-            defaultMeta: { service: "auth-service" },
             transports: this.getTransports(),
         });
     }
--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,67 +1,62 @@
-import { addColors, createLogger, transports, format } from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
+import { addColors, createLogger, transports, format } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 const { combine, timestamp, colorize, printf } = format;
 
 class Logger {
-  private levels = { error: 0, warn: 1, info: 2, http: 3, debug: 4 };
   private colors = {
-    error: "red",
-    warn: "yellow",
-    info: "green",
-    http: "magenta",
-    debug: "white",
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    http: 'magenta',
+    debug: 'white',
   };
-  private env = process.env.NODE_ENV || "development";
-  private isDevelopment = this.env === "development";
-
-  constructor() {
-    addColors(this.colors);
-  }
-
-  private level() {
-    return this.isDevelopment ? "debug" : "warn";
-  }
+  private env = process.env.NODE_ENV || 'development';
+  private isDevelopment = this.env === 'development';
 
   private logFormat() {
     return combine(
-      timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
       colorize({ all: true }),
       printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
     );
   }
 
   private getTransports() {
+    const commonTransporOptions = {
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '50m',
+      maxFiles: '1d',
+    };
     const transport1: DailyRotateFile = new DailyRotateFile({
-      filename: "logs/app-%DATE%.log",
-      auditFile: "logs/app.json",
-      datePattern: "YYYY-MM-DD-HH",
-      zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "1d",
+      level: 'info',
+      filename: 'logs/app-%DATE%.log',
+      auditFile: 'logs/app.json',
+      ...commonTransporOptions,
     });
+
     const transport2: DailyRotateFile = new DailyRotateFile({
-      level: "http",
-      filename: "logs/request-%DATE%.log",
-      auditFile: "logs/request.json",
-      datePattern: "YYYY-MM-DD-HH",
-      zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "1d",
+      level: 'error',
+      filename: 'logs/error-%DATE%.log',
+      auditFile: 'logs/error.json',
+      ...commonTransporOptions,
     });
-    const transport3 = new transports.File({
-      level: "error",
-      filename: "logs/error.log",
+
+    const transport3: DailyRotateFile = new DailyRotateFile({
+      filename: 'logs/other-%DATE%.log',
+      auditFile: 'logs/other.json',
+      ...commonTransporOptions,
     });
+
     return [new transports.Console(), transport1, transport2, transport3];
   }
 
   public createLogger() {
+    addColors(this.colors);
     return createLogger({
-      level: this.level(),
-      levels: this.levels,
+      level: this.isDevelopment ? 'debug' : 'warn',
       format: this.logFormat(),
-      defaultMeta: { service: "auth-service" },
       transports: this.getTransports(),
     });
   }
