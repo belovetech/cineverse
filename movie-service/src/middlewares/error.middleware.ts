323081b020a1e6f77b951c7c84f749cd5b8b61a3
belovetech <belovetech@gmail.com>
2023-07-28 18:01:06 +0100

    [Update]: global error handler

---
+++ movie-service/src/middlewares/error.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { Exception } from '@cineverse/exceptions';
+import { logger } from '@cineverse/logger';
+
+const errorMiddleware = (err: Exception, req: Request, res: Response, next: NextFunction) => {
+  try {
+    const statusCode: number = err.statusCode || 500;
+    const message: string = err.message || 'Internal Server Error';
+    const name: string = err.name || 'InternalServerError';
+
+    logger.info(`[${req.method}] ${req.path} >> StatusCode:: ${statusCode}, message:: ${message}`);
+
+    return res.status(statusCode).json({ statusCode, name, error: message });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export default errorMiddleware;
