92ef0677f3a76e387fa248c1111139b919c223a1
belovetech <belovetech@gmail.com>
2023-07-28 16:03:56 +0100

    [Update]: create movie controller

--- movie-service/src/controllers/movie.controller.ts
+++ movie-service/src/controllers/movie.controller.ts
@@ -6,7 +6,7 @@ export default class MovieController {
   public async createMovie(req: Request, res: Response, next: NextFunction) {
     try {
       const movieData: MovieDto = req.body;
-      const movie = MovieService.createMovie(movieData);
+      const movie = await MovieService.createMovie(movieData);
       return res.status(201).json(movie);
     } catch (error) {
       console.log(error);
