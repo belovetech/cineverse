4ada44cb3a991a7b26e73e8330850d3cabddb6af
belovetech <belovetech@gmail.com>
2023-07-29 08:28:40 +0100

    [Update]: use response formatter class

--- movie-service/src/controllers/movie.controller.ts
+++ movie-service/src/controllers/movie.controller.ts
@@ -1,16 +1,20 @@
-import { MovieDto } from '@dtos/movie.dto';
-import MovieService from '@services/movie.service';
 import { Request, Response, NextFunction } from 'express';
+import { MovieDto } from '@dtos/movie.dto';
+import { logger } from '@cineverse/logger';
+import { movieLinks } from '@utils/links';
+import ApiResponseFormatter from '@utils/formatter';
+import MovieService from '@services/movie.service';
 
 export default class MovieController {
   public async createMovie(req: Request, res: Response, next: NextFunction) {
     try {
       const movieData: MovieDto = req.body;
       const movie = await MovieService.createMovie(movieData);
-      return res.status(201).json(movie);
+      const response = new ApiResponseFormatter<MovieDto>(movie, movieLinks.post);
+      return res.status(201).json(response.format());
     } catch (error) {
-      console.log(error);
-      next(error);
+      logger.error(`Create Movie Error: ${error}`);
+      return next(error);
     }
   }
 }
