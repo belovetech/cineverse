8a51d89f014efdc665504507a256588a05476367
belovetech <belovetech@gmail.com>
2023-08-27 16:51:53 +0100

    [Feat]: setup vitest config and write test for logger

--- libs/__tests__/logger.spec.ts
+++ libs/__tests__/logger.spec.ts
@@ -1,13 +1,44 @@
-import { expect, it } from 'vitest';
+import { expect, describe } from 'vitest';
 import Logger from '../src/logger';
 
-describe('logger', () => {
-  let logger: Logger;
-  before(() => {
-    logger = new Logger();
+describe.concurrent('#Logger', () => {
+  describe('#loggerInstance', () => {
+    it('logger should be defined', () => {
+      const logger: Logger = new Logger();
+      expect(logger).toBeDefined();
+    });
   });
 
-  it('should be defined', () => {
-    expect(logger).toBeDefined();
+  describe('#createLogger', () => {
+    it('should be defined', () => {
+      const logger = new Logger().createLogger();
+      expect(logger).toBeDefined();
+    });
+  });
+
+  describe('#logFormat', () => {
+    it('should return the correct log format', () => {
+      const loggerInstance = new Logger();
+      const logFormat = loggerInstance['logFormat']();
+      expect(
+        logFormat['_isFormatInstance'],
+        'Expected logFormat to be a Format instance'
+      );
+    });
+  });
+
+  describe('#getTransports', () => {
+    it('should return an array of transports', () => {
+      const loggerInstance = new Logger();
+      const transports = loggerInstance['getTransports']();
+      expect(
+        Array.isArray(transports),
+        'Expected getTransports to return an array'
+      );
+      expect(
+        transports.length > 0,
+        'Expected at least one transport in the array'
+      );
+    });
   });
 });
