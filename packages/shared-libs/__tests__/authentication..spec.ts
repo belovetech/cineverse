f5d3509befc9a7af5f3a5a6765a686678bdd5c44
belovetech <belovetech@gmail.com>
2023-09-04 23:31:00 +0100

    feat(authentication): add authentication package

---
+++ packages/shared-libs/__tests__/authentication..spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect } from 'vitest';
+import { generateToken, verifyToken } from '../src/';
+
+describe('#Authentication', () => {
+  let token: string;
+
+  describe('generateToken', () => {
+    it('should return a token', () => {
+      token = generateToken({ username: 'test' }, 'secret');
+      expect(token).toBeDefined();
+    });
+  });
+
+  describe('verifyToken', () => {
+    it('should return a payload', async () => {
+      const payload = await verifyToken(token, 'secret');
+      expect(payload.exp).toBeDefined();
+      expect(payload.iat).toBeDefined();
+      expect(payload.username).toEqual('test');
+    });
+  });
+});
