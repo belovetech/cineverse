fcb32582a8a69f04d5ec262c4b7178d3bd710a0e
belovetech <belovetech@gmail.com>
2023-09-11 15:20:56 +0100

    feat(validator): add generic dto validator and its test

--- ticket-service/vite.config.ts
+++ ticket-service/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['__tests__/**/*.spec.ts'],
+    include: ['tests/**/*.spec.ts'],
     passWithNoTests: true,
     environment: 'node',
   },
