2c68e928457e84a95951c199fd975c0ca0e71ead
belovetech <belovetech@gmail.com>
2023-09-11 12:41:48 +0100

    chore(repositories):update ticket and booking repositories

--- ticket-service/src/models/ticket.ts
+++ ticket-service/src/models/ticket.ts
@@ -5,7 +5,6 @@ import {
   Model,
   DataType,
   CreatedAt,
-  DeletedAt,
   ForeignKey,
   BelongsTo,
   Default,
--- ticket-service/src/repositories/ticket.ts
+++ ticket-service/src/repositories/ticket.ts
@@ -5,4 +5,16 @@ export class TicketRepository {
   public async create(payment: CreateTicketDto): Promise<Ticket> {
     return await Ticket.create(payment);
   }
+
+  public async findByPk(ticketId: string): Promise<Ticket | null> {
+    return await Ticket.findByPk(ticketId);
+  }
+
+  public async update(
+    ticketId: string,
+    options: Partial<Ticket>,
+  ): Promise<Ticket> {
+    Ticket.update({ ...options }, { where: { ticketId } });
+    return await this.findByPk(ticketId);
+  }
 }
