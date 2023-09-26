d3b5662891ff6284ec07902d9120a79028f70b15
belovetech <belovetech@gmail.com>
2023-09-26 09:47:23 +0100

    (chore): fix rabbitmq connection error

--- packages/shared-libs/__tests__/message.queue.spec.ts
+++ packages/shared-libs/__tests__/message.queue.spec.ts
@@ -1,33 +1,37 @@
 import { expect, describe, it, beforeAll, afterAll } from 'vitest';
 import { MessageQueue } from '../src';
 
-describe('MessageQueue', () => {
-  let messageQueue: MessageQueue;
+describe(
+  'MessageQueue',
+  () => {
+    let messageQueue: MessageQueue;
 
-  beforeAll(() => {
-    messageQueue = new MessageQueue('amqp://localhost');
-  });
+    beforeAll(() => {
+      messageQueue = new MessageQueue('amqp://localhost');
+    });
 
-  afterAll(() => {
-    messageQueue.closeConnection();
-  });
+    afterAll(() => {
+      messageQueue.closeConnection();
+    });
 
-  it('should bind exchange with queue', async () => {
-    await messageQueue.bindExchangeWithQueue('Booking-Ticket', 'Booking');
-    expect(messageQueue.queue).toBeDefined();
-    expect(messageQueue.exchange).toBeDefined();
-  });
+    it('should bind exchange with queue', async () => {
+      await messageQueue.bindExchangeWithQueue('Booking-Ticket', 'Booking');
+      expect(messageQueue.queue).toBeDefined();
+      expect(messageQueue.exchange).toBeDefined();
+    });
 
-  it('should send and receive message', async () => {
-    const message = { data: 'hello world' };
-    await messageQueue.sendMessage(message);
-    const receivedMessage = await messageQueue.getMessage();
-    expect(receivedMessage).toEqual(message);
-  });
+    it('should send and receive message', async () => {
+      const message = { data: 'hello world' };
+      await messageQueue.sendMessage(message);
+      const receivedMessage = await messageQueue.getMessage();
+      expect(receivedMessage).toEqual(message);
+    });
 
-  it('should send message with retry', async () => {
-    const message = { data: 'hello world' };
-    const result = await messageQueue.sendMessageWithRetry(message, 3);
-    expect(result).toEqual('DONE');
-  });
-});
+    it('should send message with retry', async () => {
+      const message = { data: 'hello world' };
+      const result = await messageQueue.sendMessageWithRetry(message, 3);
+      expect(result).toEqual('DONE');
+    });
+  },
+  { timeout: 20000 }
+);
